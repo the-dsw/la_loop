10c60463c598196c50132f1e77ea065bc8b22b0c
IbanGF <ueqiban@gmail.com>
2016-03-31 18:33:50 +0200

    routes and views working base

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,10 +1,16 @@
 function routes($routeProvider) {
+    for (var i = 1; i < 4; i++) {
+        $routeProvider.when('/inscription/inscription-' + i, {
+            templateUrl: 'views/inscription/inscription-' + i + '.html',
+            controller: 'subscribeController'
+        });
+    }
     $routeProvider
         .when('/', {
-            templateUrl: 'views/inscription/inscription.html',
+            templateUrl: 'views/inscription/inscription-1.html',
             controller: 'subscribeController'
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/inscription/inscription-1'
         });
 }
